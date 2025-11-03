c761876a87b0256e3a391a441e780144b9202b2c
mshupliakou <mikhail.shupliakou@gmail.com>
2025-11-03 17:47:03 +0100

    Initial commit

---
+++ src/test/java/com/project_agh/payrollmanagementsystem/PayrollManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_agh.payrollmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
