9d797fc3d7d08cfee778e706291e1998d57de44d
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-08 11:59:30 +0100

    delete and edit methods were added

--- src/main/java/com/project_agh/payrollmanagementsystem/controller/DepartmentManagementController.java
+++ src/main/java/com/project_agh/payrollmanagementsystem/controller/DepartmentManagementController.java
@@ -1,6 +1,7 @@
 package com.project_agh.payrollmanagementsystem.controller;
 
 import com.project_agh.payrollmanagementsystem.dtos.CreateDepartmentDto;
+import com.project_agh.payrollmanagementsystem.dtos.RoleDto;
 import com.project_agh.payrollmanagementsystem.repositories.DepartmentRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -8,7 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 /**
  * Controller responsible for managing department-related administrative operations.
@@ -73,6 +78,59 @@ public class DepartmentManagementController {
             );
         }
 
-        return "redirect:/dashboard";
+        return "redirect:/dashboard?tab=departments";
+    }
+
+    @PostMapping("/delete")
+    public String deleteDepartment(
+            @ModelAttribute CreateDepartmentDto departmentDto,
+            RedirectAttributes redirectAttributes,
+            HttpServletRequest request) {
+
+        try {
+            departmentRepository.deleteDepartment(
+                    departmentDto.getId()
+            );
+
+            redirectAttributes.addFlashAttribute(
+                    "successMessage",
+                    "Department has been deleted successfully."
+            );
+
+        } catch (Exception e) {
+            System.err.println("Error deleting role: " + e.getMessage());
+            redirectAttributes.addFlashAttribute(
+                    "errorMessage",
+                    "Error: Failed to delete role. Details: " + e.getMessage()
+            );
+        }
+
+
+
+        return "redirect:/dashboard?tab=departments";
+    }
+
+    @PostMapping("/edit")
+    public String editDepartment(
+            @RequestParam("id") Long id,
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            RedirectAttributes redirectAttributes) {
+
+        try {
+
+                departmentRepository.editDepartment(
+                        id, name, description
+                );
+
+            redirectAttributes.addFlashAttribute("successMessage",
+                    "Dane działa (ID: " + id + ") zostały zaktualizowane.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("errorMessage", "Error editing department: " + e.getMessage());
+        }
+
+        return "redirect:/dashboard?tab=departments";
     }
 }
