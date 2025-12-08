c95c789fa88e369d2267072a0089df4d996a54eb
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-08 12:03:25 +0100

    project entity created

---
+++ src/main/java/com/project_agh/payrollmanagementsystem/entities/Project.java
@@ -0,0 +1,36 @@
+package com.project_agh.payrollmanagementsystem.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "projekt")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+    @Id
+    @Column(name = "id_projekt", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nazwa")
+    private String name;
+
+    @Column(name = "opis")
+    private String description;
+
+    @Column(name = "data_rozpoczecia")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate projectBeginDate;
+
+    @Column(name = "data_zakonczenia")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate projectEndDate;
+
+}
