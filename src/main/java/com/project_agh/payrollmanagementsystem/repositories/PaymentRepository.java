074d725301d94acf823434255ab0740f4c7dcba6
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-23 20:27:42 +0100

    Payment Repository was created

---
+++ src/main/java/com/project_agh/payrollmanagementsystem/repositories/PaymentRepository.java
@@ -0,0 +1,18 @@
+package com.project_agh.payrollmanagementsystem.repositories;
+
+import com.project_agh.payrollmanagementsystem.entities.Payment;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentRepository {
+    List<Payment> findAll();
+
+    void createPayment();
+
+    void deletePayment(Long id);
+
+    void editPayment();
+    void updateStatus(Long id, Long statusId);
+}
