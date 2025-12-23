e3cb436422e415345b24e7781064c5dfbd5df3b3
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-23 20:28:26 +0100

    Payment Status Dto was created

---
+++ src/main/java/com/project_agh/payrollmanagementsystem/dtos/PaymentStatusDto.java
@@ -0,0 +1,10 @@
+package com.project_agh.payrollmanagementsystem.dtos;
+
+import lombok.Data;
+
+@Data
+public class PaymentStatusDto {
+    private Long id;
+    private String name;
+    private String description;
+}
